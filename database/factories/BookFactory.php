23768da88bca4cad70488ca514be95f4c8e115a5
Jesse Gerroir <jessegerroir@gmail.com>
2021-05-05 22:27:59 -0400

    project init
    project init

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition(): array
+    {
+    	return [
+            'title'      => $this->faker->words(4),
+            'author'     => $this->faker->name(),
+            'page_count' => $this->faker->randomNumber(3, false)
+    	];
+    }
+}
