d5348e4fc231138ddac2bab92b8512255ecf37ef
Jesse Gerroir <jessegerroir@gmail.com>
2021-06-01 15:46:48 -0400

    update to model
    update to model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
 
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'books';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+
+    ];
 
 }
 
